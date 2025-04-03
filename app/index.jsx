e0dcd894927349709af2803a49549443ff856878
Bruno Quintino <brunoquintino.tds2.senai@gmail.com>
2025-04-03 11:34:20 -0300

    feat: home

--- app/index.jsx
+++ app/index.jsx
@@ -1,29 +1,78 @@
-import { View, Text, StyleSheet } from "react-native";
-import { Link } from "expo-router";
+import React from "react";
+import { View, Text, Pressable, StyleSheet } from "react-native";
 
-export default function Screen() {
-    return (
-        <View style={styles.container}>
-            <Link href="/login" style={styles.link}>
-                <Text style={styles.linkText}>Ir para Login</Text>
-            </Link>
-            <Text style={styles.title}>Olá, Tudo bem?</Text>
-        </View>
-    );
-}
+const MyComponent = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Esta é a tela Sobre!</Text>
+
+      <View style={styles.buttonContainer}>
+        <Pressable
+          onPress={() => console.log("/login")}
+          style={({ pressed }) => [
+            styles.button,
+            pressed && styles.buttonPressed,
+          ]}
+        >
+          <Text style={styles.buttonText}>Ir para Login</Text>
+        </Pressable>
+
+        <Pressable
+          onPress={() => console.log("/termos")}
+          style={({ pressed }) => [
+            styles.button,
+            pressed && styles.buttonPressed,
+          ]}
+        >
+          <Text style={styles.buttonText}>Ir para Termos</Text>
+        </Pressable>
+
+        <Pressable
+          onPress={() => console.log("/sobre")}
+          style={({ pressed }) => [
+            styles.button,
+            pressed && styles.buttonPressed,
+          ]}
+        >
+          <Text style={styles.buttonText}>Ir para Sobre</Text>
+        </Pressable>
+      </View>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    title: {
-        fontSize: 24,
-        fontWeight: 'bold',
-    },
-    linkText: {
-        color: 'blue',
-        textDecorationLine: 'underline',
-    },
+
+
+ 
+  button: {
+    backgroundColor: "blue",
+    paddingVertical: 10,
+    paddingHorizontal: 20, // Ajuste o padding horizontal para um valor menor
+    borderRadius: 5,
+    marginHorizontal: 5,
+    flex: 1, // Faz com que os botões ocupem espaço igual
+  },
+  buttonPressed: {
+    backgroundColor: "darkblue",
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 16,
+    fontWeight: "bold",
+    textAlign: "center", // Centraliza o texto
+  },
+  buttonContainer: {
+    flexDirection: "row", // Alinha os botões em linha
+    justifyContent: "space-between", // Espaço igual entre os botões
+    padding: 10, // Adiciona um pouco de preenchimento ao redor
+  },
+
+  container: {
+    textAlign: 'center',
+    display: 'flex'
+  },
+  
 });
+
+export default MyComponent;
